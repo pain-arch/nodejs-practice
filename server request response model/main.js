a2cb7a3bebc535b3126e721aa5eedb348abd40a8
pain-arch <himulol123@gmail.com>
2021-07-15 19:33:44 +0600

    extra

--- server request response model/main.js
+++ server request response model/main.js
@@ -8,21 +8,21 @@ var server=http.createServer(function(req, res){
     // Home page url
     if (req.url=="/") {
         res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1 style={text-align:center} >This is Home page</h1>')
+        res.write('<h1>This is Home page</h1>')
         res.end();
     }
 
     // About page url
     else if (req.url=="/about") {
         res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1 style={text-align:center} >This is About page</h1>')
+        res.write('<h1>This is About page</h1>')
         res.end();
     }
     
     // Contct page url
     else if (req.url=="/contact") {
         res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1 style={text-align:center} >This is Contact page</h1>')
+        res.write('<h1>This is Contact page</h1>')
         res.end();
     }
     
